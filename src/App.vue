c582a58f343bad7fcaaeb463e01273ed15e7871d
kissrain <cjyang1123@gmail.com>
2017-07-16 23:14:47 +0800

    项目准备

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
 </script>
 
 <style>
